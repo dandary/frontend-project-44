b4b5c328c28d7573ab5f261e238e03b4adabf65f
Dandary <mukuro489@gmail.com>
2023-05-28 15:23:38 -0700

    addet brain-progression-game.js

---
+++ src/games/brain-progression-game.js
@@ -0,0 +1,26 @@
+import gameEngine from '../index.js';
+import getRandomNumber from '../utils.js';
+
+const description = 'What number is missing in the progression?';
+const lengthOfProgression = 10;
+const getProgression = () => {
+  let nextProgressionValue = getRandomNumber(0, 101);
+  const increase = getRandomNumber(1, 6);
+  const result = [];
+  for (let i = 0; i < lengthOfProgression; i += 1) {
+    result.push(nextProgressionValue);
+    nextProgressionValue += increase;
+  }
+  return result;
+};
+const getQuestionAndAnswer = () => {
+  const progression = getProgression();
+  const hiddenNumber = getRandomNumber(1, lengthOfProgression);
+  const expectedAnswer = String(progression[hiddenNumber]);
+  progression[hiddenNumber] = '..';
+  const anotherGameQuestion = progression.join(' ');
+  return [anotherGameQuestion, expectedAnswer];
+};
+export default () => {
+  gameEngine(description, getQuestionAndAnswer);
+};
